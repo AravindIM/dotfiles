1c519b1ac8528439e1127083e135a3a588174ecf
Aravind I M <aravindim@yahoo.com>
2023-10-14 15:07:25 +0530

    feat(nvim): flutter dev

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -256,6 +256,7 @@ vim.o.scrolloff = 8
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+vim.keymap.set('n', '<leader>.', ":Ex<CR>", { silent = true, desc = 'Open Explorer' })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
